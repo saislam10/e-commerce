9dedec10d503b4ec992ec0c1034ab68980df76e7
Safwan Islam <saislam@davidson.edu>
2022-06-30 00:09:40 -0500

    set default value of stock to 10

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -29,7 +29,7 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      //need to set dafault value of 10
+      defaultValue: 10,
       validate: {
         isNumeric: true, 
       }
